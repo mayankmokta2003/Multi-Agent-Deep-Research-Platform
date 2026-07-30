c65a6a19b80be19ed3e4f339df19b8caebfd2651
Mayank Mokta <mayankmokta@gmail.com>
2026-07-30 15:22:22 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes.research_routes import router as research_router
 from app.api.routes.system_routes import router as system_router
+from app.database.base import Base
+from app.database.connection import engine
+from app.models.research_model import Research
 
 app = FastAPI(
     title="ResearchOS API",
     description="Production-grade AI Research Assistant",
     version="1.0.0",
 )
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(research_router, prefix="/api/v1")
 app.include_router(system_router, prefix="/api/v1")
--- app/services/research_service.py
+++ app/services/research_service.py
@@ -1,14 +1,22 @@
 from app.graph.builder import graph
 from app.schemas.research_response import ResearchResponse
+from app.database.connection import SessionLocal
+from app.models.research_model import Research
 
 
 def run_research(query: str):
+    db = SessionLocal()
     try:
         result = graph.invoke({
             "query": query
         })
+        research = Research(query=query, report=result["final_result"])
+        db.add(research)
+        db.commit()
         return ResearchResponse(report=result["final_result"])
 
     except Exception as e:
+        db.rollback()
         raise RuntimeError(f"Research generation failed: {str(e)}")
-
+    finally:
+        db.close()
