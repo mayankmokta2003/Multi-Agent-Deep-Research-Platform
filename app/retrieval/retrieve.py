66ff2b71f8f39a606cef0675e06fe1b7c0ace9c6
Mayank Mokta <mayankmokta@gmail.com>
2026-08-08 16:04:47 +0530

    updated

--- app/retrieval/retrieve.py
+++ app/retrieval/retrieve.py
@@ -5,18 +5,18 @@ from app.config.settings import get_settings
 settings = get_settings()
 
 embeddings = MistralAIEmbeddings(api_key=settings.MISTRAL_API_KEY)
+vectorstore = Chroma(
+    persist_directory="db",
+    embedding_function=embeddings
+)
 
 
 def retrieve(query: str):
-    vectorstore = Chroma(
-        persist_directory="db",
-        embedding_function=embeddings
-    )
     docs = vectorstore.similarity_search_with_relevance_scores(query=query, k=5)
     filtered_docs = []
     for doc, score in docs:
-        print(f"Score: {score:.3f}")
+        # print(f"Score: {score:.3f}")
         if score > 0.5:
             filtered_docs.append(doc)
-    print(f"Retrieved: {len(docs)} | Filtered: {len(filtered_docs)}")        
+    # print(f"Retrieved: {len(docs)} | Filtered: {len(filtered_docs)}")        
     return filtered_docs
